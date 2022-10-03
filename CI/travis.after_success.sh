284b21436951c87359bcc44027570d57598b028b
Nikarous <maciej.lopal+github@gmail.com>
2022-10-03 19:06:40 +0200

    Check dockerize

--- CI/travis.after_success.sh
+++ CI/travis.after_success.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-if { [ -n "$TRAVIS_REPO_SLUG" ] && [ "${TRAVIS_REPO_SLUG}" != "Mudlet/Mudlet" ]; } ||
-   { [ -n "$GITHUB_REPOSITORY" ] && [ "${GITHUB_REPOSITORY}" != "Mudlet/Mudlet" ]; } then
-  exit 0
-fi
+# if { [ -n "$TRAVIS_REPO_SLUG" ] && [ "${TRAVIS_REPO_SLUG}" != "Mudlet/Mudlet" ]; } ||
+#    { [ -n "$GITHUB_REPOSITORY" ] && [ "${GITHUB_REPOSITORY}" != "Mudlet/Mudlet" ]; } then
+#   exit 0
+# fi
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ] || [ "${RUNNER_OS}" = "Linux" ]; then
   echo "Deploy on Linux."
@@ -30,22 +30,22 @@ elif [ "${TRAVIS_OS_NAME}" = "osx" ]  || [ "${RUNNER_OS}" = "macOS" ]; then
   . CI/travis.osx.after_success.sh;
 fi
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  prId=" ,#${TRAVIS_PULL_REQUEST}"
-fi
+# if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+#   prId=" ,#${TRAVIS_PULL_REQUEST}"
+# fi
 
-if [ -n "${DEPLOY_URL}" ]; then
-  curl \
-    --data-urlencode "message=Deployed Mudlet \`${VERSION}${MUDLET_VERSION_BUILD}\` (${TRAVIS_OS_NAME}${prId}) to [${DEPLOY_URL}](${DEPLOY_URL})" \
-    https://webhooks.gitter.im/e/cc99072d43b642c4673a
-fi
+# if [ -n "${DEPLOY_URL}" ]; then
+#   curl \
+#     --data-urlencode "message=Deployed Mudlet \`${VERSION}${MUDLET_VERSION_BUILD}\` (${TRAVIS_OS_NAME}${prId}) to [${DEPLOY_URL}](${DEPLOY_URL})" \
+#     https://webhooks.gitter.im/e/cc99072d43b642c4673a
+# fi
 
 echo ""
 echo "******************************************************"
 echo ""
 echo "Finished building Mudlet ${VERSION}${MUDLET_VERSION_BUILD}"
-if [ -n "${DEPLOY_URL}" ]; then
-  echo "Deployed the output to ${DEPLOY_URL}"
-fi
+# if [ -n "${DEPLOY_URL}" ]; then
+#   echo "Deployed the output to ${DEPLOY_URL}"
+# fi
 echo ""
 echo "******************************************************"
